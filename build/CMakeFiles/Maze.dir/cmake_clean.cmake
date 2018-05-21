053fc045066e3cc0a0399ae3b3241eb8c50d9435
StumboEugen <marineworld@foxmail.com>
2018-05-21 16:59:41 +0800

    backup

---
+++ build/CMakeFiles/Maze.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maze.dir/src/maze.cpp.o"
+  "Maze.pdb"
+  "Maze"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
